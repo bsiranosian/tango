bb3198b44626b659718d502975ab88ee8eda584e
Chen Ye <cye@brown.edu>
2014-04-18 03:01:00 -0400

    hello python

---
+++ src/RtoPhylip.py
@@ -0,0 +1,12 @@
+class RtoPhylip(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.file = open(filename, "r")
+        
+    def process():
+        self.rData = file.read
+    pass
+
+
+
+
