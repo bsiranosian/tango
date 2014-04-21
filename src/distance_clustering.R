e2c01d5becec96d3d5b0fde1d6d4b1f8560fa5fc
bsiranosian <benjamin_siranosian@brown.edu>
2014-04-20 22:18:41 -0400

    Some more attempts at clustering with R

--- src/distance_clustering.R
+++ src/distance_clustering.R
@@ -15,4 +15,57 @@ plot(fit4)
 # working with ape for tree building? 
 library(ape)
 njtree <- nj(d)
-  
+
+# testing things like PCA and MDS to dimension reduce/cluster
+fit <- cmdscale(d, eig=T, k=2)
+fit
+# plot solution 
+x <- fit$points[,1]
+y <- fit$points[,2]
+
+clusters2 <- sapply(rownames(tud), function (x) strsplit(x,split="\\(")[[1]][2])
+clusters <- sapply(clusters2, function (x) strsplit(x,split="\\)")[[1]][1])
+
+plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2", 
+     main="Metric  MDS",   type="n")
+text(x, y, labels = clusters, cex=.7)
+
+#nonmetric MDS
+library(MASS)
+fit<-isoMDS(d,k=2)
+x <- fit$points[,1]
+y <- fit$points[,2]
+
+clusters2 <- sapply(rownames(tud), function (x) strsplit(x,split="\\(")[[1]][2])
+clusters <- sapply(clusters2, function (x) strsplit(x,split="\\)")[[1]][1])
+
+plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2", 
+     main="Metric  MDS",   type="n")
+text(x, y, labels = clusters, cex=.7)
+
+#trying different distance metrics
+#manhattan distance
+d <- dist(tud, method='manhattan')
+fit1 <- hclust(d,method='ward.D')
+fit2 <- hclust(d,method='ward.D2')
+fit3 <- hclust(d,method='complete')
+fit4 <- hclust(d,method='single')
+par(mfrow=c(2,2))
+plot(fit1)
+plot(fit2)
+plot(fit3)
+plot(fit4)
+#MDS
+fit <- cmdscale(d, eig=T, k=2)
+# plot solution 
+x <- fit$points[,1]
+y <- fit$points[,2]
+clusters2 <- sapply(rownames(tud), function (x) strsplit(x,split="\\(")[[1]][2])
+clusters <- sapply(clusters2, function (x) strsplit(x,split="\\)")[[1]][1])
+plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2", 
+     main="Metric  MDS",   type="n")
+text(x, y, labels = clusters, cex=.7)
+#manhattan verdict:
+# hclustering seems to perform better. MDS is not better or even worse. 
+# plot of clustering saved, MDS no. 
+
