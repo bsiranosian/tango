dc22769f1f3e2cf654fe0f7914d4953285ebfc44
bsiranosian <benjamin_siranosian@brown.edu>
2014-04-27 22:11:46 -0400

    Some more motif plots

--- src/variance_tests_RC.R
+++ src/variance_tests_RC.R
@@ -19,6 +19,13 @@ table(TCGA1)
 table(TCGA2)
 # only singletons are in both tables
 sum(names(table(TCGA1)) %in% names(table(TCGA2))) + sum(names(table(TCGA2)) %in% names(table(TCGA1)))
+#look at top part of TCGA
+TCGA3 <- tudrc[tudrc[,'TCGA']<3.8,"cluster"]
+TCGA4 <- tudrc[tudrc[,'TCGA']>3.8,"cluster"]
+# Literally a perfect split between clusters.
+table(TCGA3)
+table(TCGA4)
+
 
 #B3 cluster can be differentiated just on the basis of TUD in GATC
 length(tudrc[tudrc[,'GATC']>3.5,"cluster"])
@@ -34,6 +41,25 @@ GAAG2 <- tudrc[tudrc[,'GAAG']>0.49,"cluster"]
 table(GAAG1)
 table(GAAG2)
 
+#Try CCTA
+plot(hist(tudrc[,"CCTA"],breaks=50))
+# CCTA 
+#plot(hist(tudrc[tudrc[,"cluster"]=="B1","GAAG"],breaks=50))
+CCTA1 <- tudrc[tudrc[,'CCTA']<0.55,"cluster"]
+CCTA2 <- tudrc[tudrc[,'CCTA']>0.55,"cluster"]
+CCTA3 <- tudrc[tudrc[,'CCTA']>0.7,"cluster"]
+table(CCTA1)
+table(CCTA2) #Ls and Rs come out here
+table(CCTA3) #Rs isolated further with this split
+
+#Try CTAA
+plot(hist(tudrc[,"CTAA"],breaks=50)) 
+#plot(hist(tudrc[tudrc[,"cluster"]=="B1","GAAG"],breaks=50))
+CCTA1 <- tudrc[tudrc[,'CTAA']<1.0,"cluster"]
+CCTA2 <- tudrc[tudrc[,'CTAA']>1.0,"cluster"]
+table(CCTA1)
+table(CCTA2) #Ls and come out here
+
 # are tetras normally distributed within a given cluster?
 # check 
 qqnorm(tudrc[tudrc[,"cluster"]=="B1",1])
@@ -42,3 +68,14 @@ plot(hist(tudrc[tudrc[,"cluster"]=="B1",1]))
 #For each cluster, for each TUD, how many other clusters are in the range of those values
 clusterMaxVals <- t(sapply(levels(cf), function(x) apply(tudrc[tudrc[,"cluster"]==x,1:256],2,max)))
 clusterMinVals <- t(sapply(levels(cf), function(x) apply(tudrc[tudrc[,"cluster"]==x,1:256],2,min)))
+
+#dip statistic
+library(diptest)
+tudDip <- sort(apply(tudrc[1:256], 2, function(x) dip(x)),decreasing=T)
+
+par(mfrow=c(16,16))
+par(mfcol=c(16,16), oma=c(1,1,0,0), mar=c(1,1,1,0), tcl=-0.1, mgp=c(0,0,0))
+
+for (i in 1:256){
+  plot(hist(tudrc[,i],breaks=50))
+}
