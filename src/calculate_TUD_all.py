2d1c42eee56f0fd1b8be80628c8b51f0df0130a6
Ben Siranosian <benjamin_siranosian@brown.edu>
2014-06-16 14:38:43 -0400

    more cleanup

--- src/calculate_TUD_all.py
+++
@@ -1,6 +0,0 @@
-#calculate TUD for all 663 phages as of 2014-04-20
-from tetranucleotideAnalysis import *
-
-#phageTUDCalc('../data/phagesDB/sequenced_phage_map.txt', '../data/all_phages_TUD_3.tsv', 3)
-phageTUDCalc('../data/phagesDB/sequenced_phage_map.txt', '../data/all_phages_TUD_4.tsv', 4, RC=True)
-#phageTUDCalc('../data/phagesDB/sequenced_phage_map.txt', '../data/all_phages_TUD_5.tsv', 5)
